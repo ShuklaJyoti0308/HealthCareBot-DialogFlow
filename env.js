4e10469f3abe160a2e51238d1ad0e12743ebab73
Jyoti Shukla <jyoti.shukla@vcontractor.co.za>
2021-08-04 11:54:34 +0530

    first commit

---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+        "type": "service_account",
+        "project_id": "healthcarebot-sfxt",
+        "private_key_id": "a0a837f0168af07996ccba35cf214ccbb81aa614",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCofI/Er/x9Ew4X\nG2uwf++hqngV27+EQC2n11e9zJlpEMyfM32ud3EA4/RoefJ/kIfm8ytk6DkhrfqP\nMRUNq4CkoDIYIJH3BXLZSYIS/OVVMtT84klbL19VJ+6csYo7i4yVmPr4PfwM5+LP\n1k9MHtyRjwEFXpNGFBQp6tk1IPaj0XsSyU32RrCsFdLZyEFYnuywvOnVZyVWwOjh\nVKsrP65MsABbfUn5xlwpH3kcOWnHT29qTTHJSb2ZZpIkt7XoDE7uaE4Lc2neutRc\njHgRNLulDLs7qwgd7rXHlPH6j+Se/vslqs7Fqf2xfPIBTMScnnCSWidF6uZSLBMn\nKZXOXUQrAgMBAAECggEAAUenHD4oxNuyC2X4FK/GktqW0CxPezYEDxQwn6wsXipq\ngQ2Oxj2+WHonwIhY7WVNhXL+hbPd54uXt12FAjFEj6zRtJmF0RsvBz2vtgojTuzX\nAVwqZPPbexNalK2HDDfTx7j9eOtw2mq015umFfJBJcPEpPyRFsLiv2diihkWnyuj\nLcwqj7PDU7Yf6szU/5PrW2vrwSoQFd8uw7bNsP/RPmZVE7XRMNorkY/r2/Se0Q8Q\nfYFiDcBJcdLFKc+hf5AoUT8+HpG5c5ydCWJyjIDDL2onAgJgLTXq2M6vR/UhR8io\nQMG77JFltZdXOd99ICklWi71izwM1aQlPOBLsu4pAQKBgQDtsV0PI2CmnssQXSc0\nsZ3+rDyOWDS3adi1JcJ41Pi1mmffI7v2FfH0LGib1fq6OEEChD2x5pGS/rcc3nMg\nmH6oUC+ry6XINOx2cxjn9Q+Ol9zgj3qDAPe6tTHR20h+rcZDWGVc6YjvtSQ4wdYr\nbPABNuVlJZbqLjZxXXk/QUupewKBgQC1dqUFJCLR/vBKZZ5OGllKH8Uh0Eb8/Wv9\nFMq/4xu7Rkb5j9HLPTx08MstPSNP6+ne/62hPZmRoL7aFAUs+8trCbh8IsVkscNG\nQlHldKC/eOE9KRrTzZGwuzYlQCKv/ncJqmPykyShVYRyX8SkIB2EvtD7iiYQyect\ncgmYSz4ZEQKBgQDBx2g7sDbaf/jC5X7SCb/p4RcZYTXRFdzTie4qvk3pF/F4zFQv\ngqKX0yCRc3iOGtB9Mbxuba/erAQy5/Pp7gjMRVsn7sXAG81vn9P9Px+ou4waEz7o\n0IAmWhQ8p05J4HgTbVxtzAKUkhPupbBqBM0B4HRR6rxHPNerwm3XsO7zQwKBgQCp\nBHM70KamFeDWSsQiVyRniCPjFM2qApk8xsr5450G17Rv6mcr+EOBYG6+Gj3zyPay\ngE7G6ktJewGIujD/7x9O+WTeKrzUx0XtAQSqxfCbpwmUH98e/FAA3r7V/2DPsCJL\n3Grvm+H4a2DL7yuG+2rDI8AfKlO+O3CZwafyM9MJIQKBgHE/bBrFoKDujT1GRzug\nvPuebHyD7xc3IR39iOGBCmVI5/hqEzsXX0WvzCniFNMH3YVr8VbDaMWhtNlDpv5R\nY8FCD408O+QYdD+FgBvufDV9NGzVQg9bCbQ5WEOhX9HHNy/uNS7yvdgrMRSDOOV4\n6lzry09ALudSXgC71J7n7SL+\n-----END PRIVATE KEY-----\n",
+        "client_email": "healthcare-chatbot@healthcarebot-sfxt.iam.gserviceaccount.com",
+        "client_id": "109314358408930496446",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/healthcare-chatbot%40healthcarebot-sfxt.iam.gserviceaccount.com"
+      }
+      
